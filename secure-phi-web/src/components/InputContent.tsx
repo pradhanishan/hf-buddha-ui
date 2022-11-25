7646aa2b0d930350c93d19e0369a5816e547a3fb
Pradhan <ishan.pradhan@cotiviti.com>
2022-11-25 14:53:47 +0545

    before deploy

--- secure-phi-web/src/components/InputContent.tsx
+++ secure-phi-web/src/components/InputContent.tsx
@@ -7,59 +7,44 @@ const InputContent: FC = () => {
   const [fileInput, setFileInput] = useState<{
     content: any | null;
     isFileAdded: boolean;
-    fileName: string;
   }>({
     content: null,
     isFileAdded: false,
-    fileName: "",
   });
 
   const handleFileUpload = (event: any) => {
     setFileInput({
       content: event.target.files[0],
       isFileAdded: true,
-      fileName: event.target.files[0].name,
     });
   };
 
   // handle what to do when file is submitted
   const handleSubmitFile = (event: any) => {
     event.preventDefault();
-  };
-
-  // handle what to do when user presses
-  const handleClearFileInput = () => {
-    setFileInput({
-      content: null,
-      isFileAdded: false,
-      fileName: "",
-    });
+    console.log(fileInput);
   };
 
   return (
     <div className={classes["input-content"]}>
+      <h5>Add Image of PDF for scan</h5>
+      <hr />
       <input
         type="file"
         accept="image/jpeg,application.pdf,image/png"
         className="mb-3"
         onChange={handleFileUpload}
-        value={fileInput.fileName}
       />
       <br />
-      {fileInput.isFileAdded && (
-        <>
-          <Button variant="success" type="submit" onClick={handleSubmitFile}>
-            Upload
-          </Button>
-          <Button
-            variant="danger"
-            className="mx-2"
-            onClick={handleClearFileInput}
-          >
-            Clear
-          </Button>
-        </>
-      )}
+
+      <Button
+        variant="success"
+        type="submit"
+        onClick={handleSubmitFile}
+        disabled={!fileInput.isFileAdded}
+      >
+        Scan
+      </Button>
 
       {/* <ProgressBar animated now={45} /> */}
     </div>
